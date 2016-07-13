5525b289b5a4730b2dd72974b4b35be359c5c904
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-07-13 08:51:47 +0100

    changes

--- fruit-machine/lib/fruitmachine.rb
+++ fruit-machine/lib/fruitmachine.rb
@@ -1,3 +1,5 @@
+
+
 class FruitMachine
 
   attr_reader :colours, :player_turn, :wheel
@@ -8,14 +10,17 @@ class FruitMachine
   # WHEEL_THREE = [:red, :green, :blue, :yellow]
   # WHEEL_FOUR = [:red, :green, :blue, :yellow]
 
-  def initialize gambler
+  def initialize gambler = Gambler , rules = Rules
     @wheel = WHEEL
     @gambler = gambler
+    @rules = rules
     # @wheel_two = WHEEL_TWO
     # @wheel_three = WHEEL_THREE
     # @wheel_four = WHEEL_FOUR
     @player_turn = []
   end
+
+  #add the wheel to a module and inherit it to player and fruitmachine
 
   def spin_the_wheel
     SLOTS.times { wheel_one_spin }
@@ -24,6 +29,8 @@ class FruitMachine
   def wheel_one_spin
     @player_turn << @wheel.sample
   end
+
+
   def four_in_a_row
     @player_turn.all? {|slot| slot == @player_turn[0]}
   end
@@ -35,13 +42,20 @@ class FruitMachine
     @gambler.play_game coin
   end
 
+  def win_coin coin
+    @gambler.deposit coin
+  end
+
   def two_or_more_adjacent
     @player_turn.each_cons(2).any? {|slot_a, slot_b| slot_a == slot_b }
   end
 
-
-  # def win_coins
-  #   four_in_a_row ? @gambler.deposit @rules::HALVE : @gambler.withdraw @rules::TURN
-  # end
+   #
+   def win_coins
+    four_in_a_row ? win_coin(100) : insert_coin(1)
+    two_or_more_adjacent ? win_coin(50): insert_coin(1)
+    four_different_colours ? win_coin(90): insert_coin(1)
+    # ? @gambler.deposit 5  : @gambler.withdraw
+   end
 
 end
--- fruit-machine/spec/feature.rb
+++ fruit-machine/spec/feature.rb
@@ -2,7 +2,8 @@ require './lib/fruitmachine.rb'
 require './lib/gambler.rb'
 require './lib/rules.rb'
 harry = Gambler.new
-fruity = FruitMachine.new harry
+rules = Rules.new
+fruity = FruitMachine.new harry, rules
 fruity.spin_the_wheel
 fruity.four_in_a_row
 fruity.player_turn
